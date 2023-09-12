ada16eeb0e6d2d6ae3b2a4915e817ebdfd82cb2f
GilbertoMedeiros <gilberto.medeiros@compasso.com.br>
2023-09-12 14:56:28 -0300

    feat: create files seeds user and post

---
+++ src/seeds/MainSeeder.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm'
+import { runSeeder, Seeder, SeederFactoryManager } from 'typeorm-extension'
+import { UserSeeder } from './UserSeeder'
+import { PostSeeder } from './PostSeeder'
+
+export class MainSeeder implements Seeder {
+	async run(
+		dataSource: DataSource,
+		factoryManager: SeederFactoryManager
+	): Promise<void> {
+		await runSeeder(dataSource, UserSeeder)
+		await runSeeder(dataSource, PostSeeder)
+	}
+}
